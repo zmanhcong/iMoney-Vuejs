b4ff52bcb38d19afd079cdb8623a13c055e20b27
nmcong1 <nmcong1>
2023-12-23 00:39:17 +0900

    refactor

--- src/components/NaviBrand.vue
+++ src/components/NaviBrand.vue
@@ -4,7 +4,7 @@
       <img
         src="https://images.unsplash.com/photo-1602532350069-f1e51a6420ab?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt=""
-        class="h-[330px]"
+        class="h-[330px] object-contain rounded-xl"
       />
 
       <p class="text-center mt-3">Best way to saving your Money!</p>
