cee4a5b00de6826aee5372da5505817fd0cc84ef
nmcong1 <nmcong1>
2023-12-22 16:00:17 +0900

    setting firebase

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,28 @@ import { useRoute } from 'vue-router'
 import { PUBLIC_LAYOUT } from '@/constants'
 import { computed } from 'vue'
 
+import { fireStoreCore } from '@/configs/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+
 export default {
   name: 'MainApp',
 
   setup() {
     const route = useRoute()
     console.log(route.meta.layout)
+
+    // setting firebase
+    async function getTransactions() {
+      const querySnapshot = await getDocs(collection(fireStoreCore, 'transactions'))
+      querySnapshot.forEach((doc) => {
+        const docDataFormatted = {
+          id: doc.id,
+          ...doc.data()
+        }
+        console.log(docDataFormatted)
+      })
+    }
+    getTransactions()
 
     return {
       layout: computed(() => (route.meta.layout || PUBLIC_LAYOUT) + '-layout')
